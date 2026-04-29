0738c66a6e8ea985b19b41154917a6b651c9c7e9
Lilian Kasem <likasem@microsoft.com>
2026-04-29 11:31:23 -0700

    Migrate from Application Insights to OpenTelemetry

--- src/Program.cs
+++ src/Program.cs
@@ -3,13 +3,16 @@ using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Azure.Monitor.OpenTelemetry.Exporter;
+using Microsoft.Azure.Functions.Worker.OpenTelemetry;
+using OpenTelemetry;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
 
-builder.Services
-       .AddApplicationInsightsTelemetryWorkerService()
-       .ConfigureFunctionsApplicationInsights();
+builder.Services.AddOpenTelemetry()
+    .UseFunctionsWorkerDefaults()
+    .UseAzureMonitorExporter();
 
 builder.Build().Run();
